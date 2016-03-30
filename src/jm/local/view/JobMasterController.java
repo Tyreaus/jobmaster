009d51c99a367b7cb28754f65be64e5efbacd568
Tyreaus <dtravislee@gmail.com>
2016-03-30 13:50:04 -0700

    Pie Chart on dashboard now populates

--- src/jm/local/view/JobMasterController.java
+++ src/jm/local/view/JobMasterController.java
@@ -1,8 +1,12 @@
 package jm.local.view;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.geometry.Side;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.PieChart;
+import javafx.scene.chart.PieChart.Data;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
@@ -28,6 +32,9 @@ public class JobMasterController {
 	@FXML
 	private TableView dashboardTable;
 	
+	ObservableList<Data> pieChartData;
+	ObservableList<Data> barChartData;
+	
 	//Reference to main application
 	
 	private Main mainApp;
@@ -50,8 +57,55 @@ public class JobMasterController {
     	viewComboBox.setOnAction((event) -> {
     	    String selectedView = viewComboBox.getSelectionModel().getSelectedItem();
     	    
+    	    if(selectedView.equals("Clients")){
+    	    	setPieChart();
+    	    	setBarChart();
+    	    }
+    	    else if(selectedView.equals("Jobs")){
+    	    	
+    	    }
+    	    else if(selectedView.equals("Employees")){
+    	    	
+    	    }
+    	    else{
+    	    	
+    	    }
     	});
     	
+    }//end Initialize
+    
+    private void setPieChart(){
+    	//Get data from the database based on the query
+    	//NOTE: May be able to pass selectedView, line 56,
+    	//	to this method and use it to get a query
+    	//	that can be parsed into data obj.s
+    	
+    	//Create the pie chart items
+    	pieChartData = FXCollections.observableArrayList(
+    			new Data("Item1", 25),
+    			new Data("Item2", 75)
+    			);
+    	
+    	//Set the chart data to the pie chart
+    	byIndChart.setData(pieChartData);
+    	byIndChart.setTitle("Clients By Industry");
+    }
+    
+    private void setBarChart(){
+    	//Get data from the database based on the query
+    	//NOTE: May be able to pass selectedView, line 56,
+    	//	to this method and use it to get a query
+    	//	that can be parsed into data obj.s
+    	
+    	//Create the bar chart items
+    	barChartData = FXCollections.observableArrayList(
+    			new Data("Item1", 25),
+    			new Data("Item2", 75)
+    			);
+    	
+    	//Set the chart data to the bar chart
+    	top10Chart.setData(barChartData);
+    	top10Chart.setTitle("Top 10 Clients");
     }
     
     /**
