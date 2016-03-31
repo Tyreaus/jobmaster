874b051340d56121eedd14edc21f1aa235131474
jnad-red007 <jnadeau001@gmail.com>
2016-03-30 18:58:11 -0700

    Added the new files to the "Index"

---
+++ src/jm/local/view/JobMasterGUIController.java
@@ -0,0 +1,55 @@
+package jm.local.view;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.geometry.Side;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.PieChart.Data;
+import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Series;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ComboBox;
+import jm.local.Main;
+
+public class JobMasterGUIController {
+
+	
+	
+	private Main mainApp;
+	
+    /**
+     * The constructor.
+     * The constructor is called before the initialize() method.
+     */
+    public JobMasterGUIController() {
+    }
+    
+    /**
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+    @FXML
+    private void initialize() {
+    	
+    	
+    	
+    }//end Initialize
+    
+    
+    
+    /**
+     * Is called by the main application to give a reference back to itself.
+     * 
+     * @param mainApp
+     */
+    public void setMainApp(Main mainApp) {
+        this.mainApp = mainApp;
+    }//end setMainApp
+	
+}//end JobMasterController
